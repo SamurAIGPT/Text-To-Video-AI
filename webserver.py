8201e742f99cdd11118601a217549349fa843dbc
Anil-matcha <matcha72@gmail.com>
2024-06-06 18:09:41 +0530

    Take input topic from command line

--- webserver.py
+++ webserver.py
@@ -10,29 +10,38 @@ from utility.captions.timed_captions_generator import generate_timed_captions
 from utility.video.background_video_generator import generate_video_url
 from utility.render.render_engine import get_output_media
 from utility.video.video_search_query_generator import getVideoSearchQueriesTimed
+import argparse
 
-#Testing Variables
-SAMPLE_TOPIC = "historical facts"
-SAMPLE_FILE_NAME = "audio_tts.wav"
-VIDEO_SERVER = "pexel"
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Generate a video from a topic.")
+    parser.add_argument("topic", type=str, help="The topic for the video")
 
-response = generate_script(SAMPLE_TOPIC)
-print("script: {}".format(response))
+    args = parser.parse_args()
+    SAMPLE_TOPIC = args.topic
+    print("Inf", SAMPLE_TOPIC)
+    SAMPLE_FILE_NAME = "audio_tts.wav"
+    VIDEO_SERVER = "pexel"
 
-asyncio.run(generate_audio(response, SAMPLE_FILE_NAME))
+    response = generate_script(SAMPLE_TOPIC)
+    print("script: {}".format(response))
 
-timed_captions = generate_timed_captions(SAMPLE_FILE_NAME)
-print(timed_captions)
-search_terms = getVideoSearchQueriesTimed(response,timed_captions)
-print(search_terms)
-background_video_urls = None
-if search_terms!=None:
-    background_video_urls = generate_video_url(search_terms,VIDEO_SERVER)
-    print(background_video_urls)
-else:
-    print("No background video")
-if  background_video_urls!=None:  
-    video = get_output_media(SAMPLE_FILE_NAME,timed_captions,background_video_urls,VIDEO_SERVER)
-    print(video)
-else:
-    print("No video")
+    asyncio.run(generate_audio(response, SAMPLE_FILE_NAME))
+
+    timed_captions = generate_timed_captions(SAMPLE_FILE_NAME)
+    print(timed_captions)
+
+    search_terms = getVideoSearchQueriesTimed(response, timed_captions)
+    print(search_terms)
+
+    background_video_urls = None
+    if search_terms is not None:
+        background_video_urls = generate_video_url(search_terms, VIDEO_SERVER)
+        print(background_video_urls)
+    else:
+        print("No background video")
+
+    if background_video_urls is not None:
+        video = get_output_media(SAMPLE_FILE_NAME, timed_captions, background_video_urls, VIDEO_SERVER)
+        print(video)
+    else:
+        print("No video")
